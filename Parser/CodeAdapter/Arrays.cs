2c347928ce8b9c38f242926a9ce3112495fa385b
kt5ov <calgoconverter@gmail.com>
2013-07-05 21:30:09 +0300

    replace arrays to Mq4Arrays

--- Parser/CodeAdapter/Arrays.cs
+++ Parser/CodeAdapter/Arrays.cs
@@ -5,39 +5,8 @@ namespace _2calgo.Parser.CodeAdapter
 {
     public static class Arrays
     {
-        private static readonly Regex OldArrayDeclarationRegex = new Regex(
-            @"(?<type>\w+)\s+(?<name>\w+)\s*\[(?<size>[^\]]*)\]\s*(\=\s*(?<init>\{[^\}]*\})\s*){0,1};",
-            RegexOptions.Compiled);
-
-        public static string FixArrayDeclarations(this string code)
-        {
-            foreach (var match in OldArrayDeclarationRegex.Matches(code).OfType<Match>())
-            {
-                var variable = match.Groups["name"].Value;
-                var type = match.Groups["type"].Value;
-                var size = match.Groups["size"].Value.Trim();
-                var init = match.Groups["init"].Value.Trim();
-
-                string replacement;
-                if (size == string.Empty)
-                {
-                    var initReplacement = init == string.Empty
-                                              ? string.Empty
-                                              : string.Format(" = new {0}[] {1}", type, init);
-                    replacement = string.Format("{0}[] {1}{2};", type, variable, initReplacement);
-                }
-                else
-                {
-                    replacement = string.Format("{0}[] {1} = new {0}[{2}]{3};", type, variable, size, init);
-                }
-
-                code = code.Replace(match.Value, replacement);
-            }
-            return code;
-        }
-        
         private static readonly Regex ArrayDeclarationRegex = new Regex(
-            @"(?<type>\w+)\s+(?<name>\w+)\s*\[(?<size>[^\]]*)\](?<initialization>\s*\=\s*(?<values>\{.*\})){0,1}", RegexOptions.Compiled);
+            @"(?<type>\w+)\s+(?<name>\w+)\s*\[(?<size>[^\]]*)\](?<initialization>\s*\=[\s\n\r]*(?<values>\{[^\}]*\})){0,1}", RegexOptions.Compiled);
 
         public static string ReplaceArraysToMq4Arrays(this string code)
         {
