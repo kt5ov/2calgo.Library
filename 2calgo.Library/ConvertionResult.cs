87da4f9fae40f4bc5c64a13f3e2644bf2dea10cf
kt5ov <calgoconverter@gmail.com>
2013-08-25 18:14:47 +0300

    add IsSuccessfull property to ConversionResult

--- 2calgo.Library/ConvertionResult.cs
+++ 2calgo.Library/ConvertionResult.cs
@@ -1,6 +1,7 @@
 ﻿using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using _2calgo.Parser.Errors;
+using System.Linq;
 
 namespace _2calgo.Library
 {
@@ -9,12 +10,17 @@ namespace _2calgo.Library
         public IEnumerable<ParsingError> ParsingErrors { get; private set; }
         public IEnumerable<CompilerError> CompilerErrors { get; private set; }
         public string CAlgoCode { get; private set; }
-
+        
         public ConvertionResult(string cAlgoCode, IEnumerable<ParsingError> parsingErrors, IEnumerable<CompilerError> compilerErrors)
         {
             ParsingErrors = parsingErrors;
             CompilerErrors = compilerErrors;
             CAlgoCode = cAlgoCode;
+        }
+
+        public bool IsSuccessfull
+        {
+            get { return !(CompilerErrors.Any(e => !e.IsWarning) || ParsingErrors.Any(e => e.ErrorType >= ErrorType.Error)); }
         }
     }
 }
