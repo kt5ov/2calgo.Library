3f80355f16794a27309c47d4e844761583a34f5f
kt5ov <calgoconverter@gmail.com>
2013-09-30 22:07:20 +0300

    support Order select by pos and OrderLots

--- Presenter/Templates/Common/InnerParts/Account/Orders.c
+++ Presenter/Templates/Common/InnerParts/Account/Orders.c
@@ -3,7 +3,7 @@ const int SELECT_BY_POS = 0;
 
 
 [Conditional("OrdersTotal")]
-int OrdersTotal()
+Mq4Double OrdersTotal()
 {
 	return Account.Positions.Count + Account.PendingOrders.Count;
 }
@@ -15,10 +15,33 @@ bool OrderSelect(int index, int select, int pool = MODE_TRADES)
 {
 	_currentOrder = null;
 
+	var allOrders = Account.Positions.OfType<object>()
+							.Concat(Account.PendingOrders.OfType<object>())
+							.ToArray();
+
 	if (select == SELECT_BY_POS)
 	{
+		if (index < 0 || index >= allOrders.Length)
+			return false;
 
+		_currentOrder = allOrders[index];
 	}
 
 	return true;
+}
+
+Mq4Double OrderLots()
+{
+	if (_currentOrder == null)
+		return 0;
+
+	double volume;
+
+	var currentPosition = _currentOrder as Position;
+	if (currentPosition != null)
+		volume = currentPosition.Volume;
+	else
+		volume = ((PendingOrder)_currentOrder).Volume;
+
+	return volume / 100000;
 }
